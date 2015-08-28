abbfc349659b0a564ef0304d0be2b7187be53c62
facehead1992 <praytoplaygames@gmail.com>
2015-08-28 02:22:46 -0500

    Created main.cpp. Used includes from entityx example. commented out sfml & entityx content. Attempting Hello World cout. Having linker error, possibly because it's a win32 application without a main window object of some kind.

---
+++ Raven2015/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * Created By: Will Nations
+ * 
+ *
+ */
+
+#include <cmath>
+#include <unordered_set>
+#include <sstream>
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+//#include <SFML/Window.hpp>
+//#include <SFML/Graphics.hpp>
+//#include <entityx/entityx.h>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+//namespace ex = entityx;
+
+int main(int args, char **argc) {
+
+    cout << "Hello World" << endl;
+
+    return 0;
+}
