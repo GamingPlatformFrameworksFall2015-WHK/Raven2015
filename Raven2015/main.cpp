f68c7d0a37ba15b8808ae5346d0703ee36391880
facehead1992 <praytoplaygames@gmail.com>
2015-08-29 12:27:38 -0500

    add .dll's for vs2015 (from nightlybuilds website), got a compiling version of sfml content. Next planning to add entityx. Still requires updates to project settings in local branch of other users.

--- Raven2015/main.cpp
+++ Raven2015/main.cpp
@@ -12,8 +12,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
-//#include <SFML/Window.hpp>
-//#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 //#include <entityx/entityx.h>
 
 using std::cout;
@@ -22,9 +22,29 @@ using std::endl;
 
 //namespace ex = entityx;
 
-int main(int args, char **argc) {
+int main() {
 
-    cout << "Hello World" << endl;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+    shape.setFillColor(sf::Color::Cyan);
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
 }
